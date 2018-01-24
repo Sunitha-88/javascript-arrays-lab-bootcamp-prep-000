e6cebca644fe3c50a29031b40e71f830648e478a
Suni <suni.suni571@gmail.com>
2018-01-24 09:57:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-const app = "I don't do much."
+var kittens=['Milo', 'Otis', 'Garfield'];
+
+function kittens(){
+	return kittens;
+	}
+
+function destructivelyAppendKitten(name){
+	kittens();
+	kittens.push('Ralph');
+	return kittens;
+	}
+
+function destructivelyPrependKitten(name){
+	kittens();
+	kittens.unshift('Bob');
+	return kittens;
+	}
+function destructivelyRemoveLastKitten(){
+	kittens();
+	kittens.pop();
+	return kittens;
+	}
+
+function destructivelyRemoveFirstKitten(){
+	kittens();
+	kittens.shift();
+	return kittens;
+	}
+
+function appendKitten(name){
+	kittens();
+	kittens.push('Broom');
+	return kittens;
+	}
+function prependKitten(name){
+	kittens();
+	return [name,...kittens];
+	}
+function removeLastKitten(){
+	kittens();
+	return slice(0,kittens.length-1);
+	}
+function removeFirstKitten(){
+	kittens();
+	return slice(1,kittens.length);
+	}
